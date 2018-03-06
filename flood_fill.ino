f3f11bd621158f8a5c6566b32c93c4b78a4530eb
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2018-03-06 11:51:29 -0800

    Add files via upload

---
+++ flood_fill.ino
@@ -0,0 +1,13 @@
+//put into brackets of the number of cells in the maze
+//using 5 * 5 for testing
+int dim_1 = 5;
+int dim_2 = 5;
+int maze [dim_1][dim_2];
+
+int target_color = 13;
+int replacement_color = 0;
+int node = 1;
+
+void floodfill(node,target_color,replacement_color){
+  
+}
