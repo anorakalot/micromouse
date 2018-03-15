c6590937bef1a223fb9a389e07333389351e62f4
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2018-03-15 13:33:31 -0700

    Add files via upload

---
+++ random_solve.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <unistd.h>
+
+std::vector<std::vector<int>> maze;
+int random_move;
+
+
+void fill_maze(int,int);
+void print_maze();
+void random_solve(int start_x,int start_y,int finish_x,int finish_y);
+
+int main(){
+
+std::cout << "Start (0,0) " <<"End at (9,9)";
+fill_maze(10,10);
+print_maze();
+
+random_solve (0,0,9,9);
+
+}
+
+void random_solve(int pos_x,int pos_y,int finish_x,int finish_y){
+
+  
+  srand(time(0));
+
+  maze.at(pos_x).at(pos_y) = 1;
+
+  while(pos_x != finish_x && pos_y != finish_y){
+    random_move = rand() %(4-1+1) + 1;
+    if (random_move == 1){
+      //get rid of
+      maze.at(pos_x).at(pos_y) = 0;
+      pos_y +=1;
+      maze.at(pos_x).at(pos_y) = 1;
+      print_maze();
+      usleep(1000);
+    }
+    else if (random_move ==2){
+
+      maze.at(pos_x).at(pos_y) = 0;
+      pos_y -=1;
+      maze.at(pos_x).at(pos_y) = 1;
+      print_maze();
+      usleep(1000);
+    }
+    else if (random_move ==3){
+
+      maze.at(pos_x).at(pos_y) = 0;
+      pos_x +=1;
+      maze.at(pos_x).at(pos_y) = 1;
+      print_maze();
+      usleep(1000);
+    }
+    else if (random_move ==4){
+
+      maze.at(pos_x).at(pos_y) = 0;
+      pos_x -=1;
+      maze.at(pos_x).at(pos_y) = 1;
+      print_maze();
+      usleep(1000);
+    }
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+void fill_maze(int y_size,int x_size){//, int y_size){
+  std::vector<int> temp;
+  for (int x = 0; x < x_size; ++x){
+    temp.push_back(0);
+  }
+
+  for (unsigned y = 0; y < y_size; ++y){
+    //for(unsigned y = 0; y <y_size; ++y){
+      //maze.at(x).at(y).push_back(0);
+    //}
+    maze.push_back(temp);
+  }
+}
+
+
+void print_maze(){
+  for (int x = 0; x < maze.size(); ++x){
+    for(int y = 0; y < maze.at(x).size(); ++y){
+      std::cout << maze.at(x).at(y) << " ";
+    }
+    std::cout << std::endl;
+  }
+}
