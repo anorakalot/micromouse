d0d6b509260a3a37b6661d40d17b37772a0dd90e
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2018-04-13 08:34:36 +0000

    Add files via upload

--- motor_test_micromouse.ino
+++ motor_test_micromouse.ino
@@ -4,15 +4,12 @@ int motor_1_logic_2 = 4;
 int motor_2_logic_1 = 5;
 int motor_2_logic_2 = 10;
 
-
 int turn_on_en_1 = 22;
 int turn_on_en_2 = 23;
 
-
-int val;
+//int val;
 
 void setup() {
- // pinMode(turn_on_en_input,INPUT);
   pinMode(turn_on_en_1,OUTPUT);
   pinMode(turn_on_en_2,OUTPUT);
   
@@ -29,38 +26,69 @@ void setup() {
   
 }
 
-void forward(){
+
+void reverse(){
   digitalWrite(turn_on_en_1,HIGH);
   digitalWrite(turn_on_en_2,HIGH);
   
   digitalWrite(motor_1_logic_1,HIGH);
   digitalWrite(motor_1_logic_2,LOW);
+  
   digitalWrite(motor_2_logic_1,HIGH);
   digitalWrite(motor_2_logic_2,LOW);
   
 }
 
-void reverse(){
+void forward(){
   
   digitalWrite(turn_on_en_1,HIGH);
   digitalWrite(turn_on_en_2,HIGH);
   
   digitalWrite(motor_1_logic_1,LOW);
   digitalWrite(motor_1_logic_2,HIGH);
+  
   digitalWrite(motor_2_logic_1,LOW);
   digitalWrite(motor_2_logic_2,HIGH);
   
 }
 
 
+void halt(){
+  digitalWrite(turn_on_en_1,LOW);
+  digitalWrite(turn_on_en_2,LOW);
+  
+  digitalWrite(motor_1_logic_1,LOW);
+  digitalWrite(motor_1_logic_2,LOW);
+  digitalWrite(motor_2_logic_1,LOW);
+  digitalWrite(motor_2_logic_2,LOW);
+    
+
+}
+
+void test(){
+  digitalWrite(turn_on_en_1,HIGH);
+  digitalWrite(turn_on_en_2,LOW);
+  
+  digitalWrite(motor_1_logic_1,HIGH);
+  digitalWrite(motor_1_logic_2,LOW);
+  digitalWrite(motor_2_logic_1,LOW);
+  digitalWrite(motor_2_logic_2,LOW);
+    
+
+}
+
 //testing loop
 void loop() {
- 
-forward();
-delay(500);
-reverse();
-delay(500);
 
-
+  forward();
+  delay(500);
+  halt();
+  delay(500);
+  reverse();
+  delay(500);
+  halt();
+  delay(500);
+//digitalWrite(turn_on_en_1,HIGH);
+//digitalWrite(turn_on_en_2,HIGH);
 }
 
