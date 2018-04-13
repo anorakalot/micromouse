4c4a9751eff66c7eaf6e395af855d7c7ca1d56bd
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2018-04-13 11:34:08 +0000

    Add files via upload

--- motor_test_micromouse.ino
+++ motor_test_micromouse.ino
@@ -1,3 +1,5 @@
+
+
 int motor_1_logic_1 = 3;
 int motor_1_logic_2 = 4;
 
@@ -77,17 +79,86 @@ void test(){
 
 }
 
+void reverse_turn(){
+  digitalWrite(turn_on_en_1,HIGH);
+  digitalWrite(turn_on_en_2,HIGH);
+
+  //halt();
+  //delay(50);
+  
+  analogWrite(motor_1_logic_1,100);
+  digitalWrite(motor_1_logic_2,LOW);
+  digitalWrite(motor_2_logic_1,LOW);
+  digitalWrite(motor_2_logic_2,100);
+  delay(100);
+  
+}
+
+
+void left_turn(){
+  digitalWrite(turn_on_en_1,HIGH);
+  digitalWrite(turn_on_en_2,HIGH);
+
+  //halt();
+  //delay(50);
+  
+  digitalWrite(motor_1_logic_1,100);
+  digitalWrite(motor_1_logic_2,LOW);
+  digitalWrite(motor_2_logic_1,LOW);
+  digitalWrite(motor_2_logic_2,100);
+  delay(140);
+  
+}
+
+
+void right_turn(){
+  digitalWrite(turn_on_en_1,HIGH);
+  digitalWrite(turn_on_en_2,HIGH);
+
+  ///halt();
+  //delay(50);
+  
+  digitalWrite(motor_1_logic_1,LOW);
+  analogWrite(motor_1_logic_2,100);
+  analogWrite(motor_2_logic_1,100);
+  digitalWrite(motor_2_logic_2,LOW);
+  delay(50);
+  
+}
+
+
+
 //testing loop
 void loop() {
 
+
+/*
   forward();
   delay(500);
+ 
   halt();
   delay(500);
   reverse();
   delay(500);
   halt();
   delay(500);
+*/
+
+
+  left_turn();
+  //delay(500);
+ 
+  halt();
+  delay(500);
+
+  /*
+  reverse();
+  delay(500);
+  halt();
+  delay(500);
+*/
+
+  
 //digitalWrite(turn_on_en_1,HIGH);
 //digitalWrite(turn_on_en_2,HIGH);
 }
