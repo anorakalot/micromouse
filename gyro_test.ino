6a46b99bbcc3a8a46edda624735fb6720eee3d4b
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2018-06-06 15:36:13 -0700

    Add files via upload

---
+++ gyro_test.ino
@@ -0,0 +1,31 @@
+#include <L3G.h>
+#include <Wire.h>
+
+L3G gyro;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Wire.begin();
+  if (!gyro.init()) {
+    Serial.println("failed to autodetect gyro type");
+    while (1);
+  }
+  gyro.enableDefault();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  gyro.read();
+  Serial.print("G ");
+  Serial.print("X: ");
+  Serial.print((int)gyro.g.x);
+  Serial.print(" Y: ");
+  Serial.print((int)gyro.g.y);
+  Serial.print(" Z: ");
+  Serial.println((int)gyro.g.z);
+
+  delay(100);
+}
