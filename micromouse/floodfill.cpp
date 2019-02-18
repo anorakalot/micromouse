520b98ebc41fbd7534cfb4c20a2c44aefde59028
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2019-02-17 16:18:06 -0800

    Add files via upload

--- micromouse/floodfill.cpp
+++ micromouse/floodfill.cpp
@@ -1,39 +1,62 @@
 #include <iostream>
 #include <stack>
 #include <utility>
+#include <cmath>
 using namespace std;
 
 struct cell{
-	bool light_wall;
+	bool left_wall;
 	bool right_wall;
 	bool top_wall;
 	bool bottom_wall;
+	int value;
 };
+
+void display_maze();
+
 
 int main(){
 stack <int> s;
 
-pair <int,int> goal_coord;
-int maze[4][4];
+pair <int,int> goal_coord(4,4);
+pair <int,int> mouse_pos (8,0);
 
-for (int x =0; x <4; ++x){
+
+int maze_x_length = 9;
+int maze_y_length = 9;
+int index_x_max = 8;
+int index_y_max = 8;
+
+cell maze[maze_y_length][maze_x_length];
+
+///*
+for (int y =0; y <index_y_max; ++y){
 	
-	for(int y = 0; y < 4; ++y) {
-		maze[x][y] = 1;
+	for(int x = 0; x < index_x_max; ++x) {
+		maze[y][x].value = abs(goal_coord.first - y) + abs(goal_coord.second-x);
 	}
 }
 
-for (int x =0; x <4; ++x){
+for (int y =0; y <index_y_max; ++y){
 	
-	for(int y = 0; y < 4; ++y) {
-		cout << maze[x][y]<< " ";
+	for(int x = 0; x < index_x_max; ++x) {
+		cout << maze[y][x].value<< " ";
 	}
 	cout << endl;
 }
 
 
 
+
 	
 	
 return 0;	
+}
+
+
+
+void display_maze(){
+
+	
+	
 }
