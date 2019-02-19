0c9a6e9154c766136a606d4b822530c613c4ed32
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2019-02-18 22:57:39 -0800

    Add files via upload

--- micromouse/floodfill.cpp
+++ micromouse/floodfill.cpp
@@ -10,13 +10,14 @@ struct cell{
 	bool top_wall;
 	bool bottom_wall;
 	int value;
+	pair<int,int> coord;
 };
 
 void display_maze();
 
 
 int main(){
-stack <int> s;
+stack <pair<int,int>> checks;
 
 pair <int,int> goal_coord(4,4);
 pair <int,int> mouse_pos (8,0);
@@ -29,7 +30,7 @@ int index_y_max = 8;
 
 cell maze[maze_y_length][maze_x_length];
 
-///*
+//set up starting cell values
 for (int y =0; y <index_y_max; ++y){
 	
 	for(int x = 0; x < index_x_max; ++x) {
@@ -44,6 +45,53 @@ for (int y =0; y <index_y_max; ++y){
 	}
 	cout << endl;
 }
+
+pair<int,int> cellcheck;
+bool not_1_greater;
+pair<int,int> min_accessible;
+//start of floodfill algorithm
+checks.push(mouse_pos);
+while(mouse_pos != goal_coord){
+	1_greater = false;
+	while(!checks.empty()){
+		cellcheck = checks.top();
+		checks.pop();
+		//assume making maze puts walls for all boundaries 
+		if (maze[cellcheck.first][cellcheck.second].left_wall != 1 && 1_greater == false){
+			if (maze[cellcheck.first][cellcheck.second -1].value +1 ==  maze[mouse_pos.first][mouse_pos.second].value){
+				1_greater = true;
+			}			
+		}
+		if (maze[cellcheck.first][cellcheck.second].right_wall != 1 && 1_greater== false){
+			if (maze[cellcheck.first][cellcheck.second +1].value +1 ==  maze[mouse_pos.first][mouse_pos.second].value){
+				1_greater = true;
+			}			
+		}
+		if (maze[cellcheck.first][cellcheck.second].top_wall != 1 && 1_greater== false){
+			if (maze[cellcheck.first-1][cellcheck.second ].value +1 ==  maze[mouse_pos.first][mouse_pos.second].value){
+				1_greater = true;
+			}			
+		}
+		if (maze[cellcheck.first][cellcheck.second].bottom_wall != 1 && 1_greater== false){
+			if (maze[cellcheck.first+1][cellcheck.second ].value +1 ==  maze[mouse_pos.first][mouse_pos.second].value){
+				1_greater = true;
+			}			
+		}		
+		if (1_greater == false){
+			
+			
+		}
+		
+		
+			
+			
+		}
+		
+	}
+	
+}
+
+
 
 
 
