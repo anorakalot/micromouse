ab92cf27bbeed95b381861c934976b9da8730ea8
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2019-02-18 23:24:19 -0800

    Add files via upload

--- micromouse/floodfill.cpp
+++ micromouse/floodfill.cpp
@@ -53,6 +53,7 @@ pair<int,int> min_accessible;
 checks.push(mouse_pos);
 while(mouse_pos != goal_coord){
 	1_greater = false;
+	minval = -1;
 	while(!checks.empty()){
 		cellcheck = checks.top();
 		checks.pop();
@@ -60,26 +61,59 @@ while(mouse_pos != goal_coord){
 		if (maze[cellcheck.first][cellcheck.second].left_wall != 1 && 1_greater == false){
 			if (maze[cellcheck.first][cellcheck.second -1].value +1 ==  maze[mouse_pos.first][mouse_pos.second].value){
 				1_greater = true;
-			}			
+			}
+			if (minval == -1){
+				
+				minval = maze[cellcheck.first][cellcheck.second-1].value;
+			}
+			else{
+				if ( maze[cellcheck.first][cellcheck.second-1].value < minval){
+					minval =  maze[cellcheck.first][cellcheck.second-1].value;
+				}
+			}
 		}
 		if (maze[cellcheck.first][cellcheck.second].right_wall != 1 && 1_greater== false){
 			if (maze[cellcheck.first][cellcheck.second +1].value +1 ==  maze[mouse_pos.first][mouse_pos.second].value){
 				1_greater = true;
+			}
+			if (minval == -1){
+				minval = maze[cellcheck.first][cellcheck.second+1].value;
+			}
+			else{
+				if ( maze[cellcheck.first][cellcheck.second+1].value < minval){
+					minval =  maze[cellcheck.first][cellcheck.second+1].value;
+				}
 			}			
 		}
 		if (maze[cellcheck.first][cellcheck.second].top_wall != 1 && 1_greater== false){
 			if (maze[cellcheck.first-1][cellcheck.second ].value +1 ==  maze[mouse_pos.first][mouse_pos.second].value){
 				1_greater = true;
+			}		
+			if (minval == -1){
+				minval = maze[cellcheck.first-1][cellcheck.second].value;
+			}
+			else{
+				if ( maze[cellcheck.first-1][cellcheck.second].value < minval){
+					minval =  maze[cellcheck.first][cellcheck.second+1].value;
+				}
 			}			
 		}
 		if (maze[cellcheck.first][cellcheck.second].bottom_wall != 1 && 1_greater== false){
 			if (maze[cellcheck.first+1][cellcheck.second ].value +1 ==  maze[mouse_pos.first][mouse_pos.second].value){
 				1_greater = true;
-			}			
+			}
+			if (minval == -1){
+				minval = maze[cellcheck.first+1][cellcheck.second].value;
+			}
+			else{
+				if ( maze[cellcheck.first+1][cellcheck.second].value < minval){
+					minval =  maze[cellcheck.first][cellcheck.second+1].value;
+				}
+			}		
 		}		
 		if (1_greater == false){
-			
-			
+			maze[cellcheck.first][cellcheck.second+1].value += 1;
+				
 		}
 		
 		
