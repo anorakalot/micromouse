d7a9bf06d5c4f705fd346193959ccbae090e8917
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2019-02-25 15:10:31 -0800

    Add files via upload

--- floodfill.cpp
+++ floodfill.cpp
@@ -1,192 +1,202 @@
-//find out how to implement wallmaze in loop
+//next thing to do is code in test mazes using the cell struct below
 #include <iostream>
-#include <vector>
-#include <queue>
 #include <stack>
-#include <cstdlib>
+#include <utility>
+#include <cmath>
 using namespace std;
 
-// struct pair {
-//    int first;
-//    int second;
-// };
-//
-//
-//    struct pair pair1;        /* Declare pair1 of type pair */
-//    struct pair pair2;        /* Declare pair2 of type pair */
+struct cell{
+	bool left_wall;
+	bool right_wall;
+	bool top_wall;
+	bool bottom_wall;
+	int value;
+	//pair<int,int> coord;
+};
 
+void display_maze();
 
 
 int main(){
-  //struct pair goals;
-  //initialize variables
-  int distmaze[8][8];
-  int wallmaze[8][8];
-  pair<int,int> goal (3,3);
-  pair<int,int> start(8,0);
-  stack<pair<int,int> > checks;
-  pair<int,int> cellcheck;
+stack <pair<int,int>> checks;
 
-  //set all walls to zero
-  for(int height = 0;height < 8; ++height){
-    for(int width = 0; width < 8; ++width){
-      wallmaze[height][width] = 0;
-    }
-  }
-
-  //FIXME find way to assign each cell with distance away from center
-  int counter = 8;
-  for(int height = 0; height < 8; ++height){
-    for(int width = 0; width < 8; ++width){
-      distmaze[height][width] = abs(goal.first - height) + abs(goal.second - width);
-    }
-    //counter--;
-  }
-
-  //output the distmaze object;
-  for(int height = 0;height < 8; ++height){
-    for(int width = 0; width < 8; ++width){
-      cout << distmaze[height][width] << " ";
-    }
-    cout << endl;
-  }
-
-  checks.push(start);
-  while(start != goal){
-    while(!checks.empty()){
-      cellcheck = checks.top();
-      checks.pop();
-      bool check_if_one_off = true;
+pair <int,int> goal_coord(4,4);
+pair <int,int> mouse_pos (8,0);
 
 
-      if (cellcheck.second -1  >= 0){
-        if (distmaze[cellcheck.first][cellcheck.second] !=
-          distmaze[cellcheck.first][cellcheck.second-1]+ 1  ){
-            check_if_one_off = false;
-          }
-        }
+int maze_x_length = 9;
+int maze_y_length = 9;
+int index_x_max = 8;
+int index_y_max = 8;
 
+cell maze[maze_y_length][maze_x_length];
 
-      if (cellcheck.first -1 >=0){
-        if (distmaze[cellcheck.first][cellcheck.second]!=
-          distmaze[cellcheck.first-1][cellcheck.second] + 1){
-            check_if_one_off = false;
-          }
-        }
+//set up starting cell values
+for (int y =0; y <index_y_max; ++y){
+	
+	for(int x = 0; x < index_x_max; ++x) {
+		maze[y][x].value = abs(goal_coord.first - y) + abs(goal_coord.second-x);
+	}
+}
 
+for (int y =0; y <index_y_max; ++y){
+	
+	for(int x = 0; x < index_x_max; ++x) {
+		cout << maze[y][x].value<< " ";
+	}
+	cout << endl;
+}
 
-      if (cellcheck.second + 1 <= 7){
-        if (distmaze[cellcheck.first][cellcheck.second]!=
-          distmaze[cellcheck.first][cellcheck.second+1] + 1){
-            check_if_one_off = false;
-          }
-        }
+pair<int,int> cellcheck;
+bool one_greater;
+int minval;
+pair<int,int> pair_to_push_to_checks;
+bool changed_mouse_pos;
 
-      if (cellcheck.first + 1 <= 7){
-        if (distmaze[cellcheck.first][cellcheck.second]!=
-          distmaze[cellcheck.first+1][cellcheck.second] + 1){
-            check_if_one_off = false;
-          }
-        }
-
-		
-		
-		
-      if (check_if_one_off == false){
-        int minval;
-		pair<int, int> next_ideal_pos;
-		
-        if (cellcheck.second -1  >= 0){
-          minval = distmaze[cellcheck.first][cellcheck.second-1];
-		  next_ideal_pos.first = cellcheck.first;
-		  next_ideal_pos.second = cellcheck.second-1;
-          }
-        }
-        if (cellcheck.first -1 >= 0){
-          if (distmaze[cellcheck.first-1][cellcheck.second] < minval){
-			minval = distmaze[cellcheck.first-1][cellcheck.second];
-		  next_ideal_pos.first = cellcheck.first-1;
-		  next_ideal_pos.second = cellcheck.second;
-          }
-        }
-		
-		if (cellcheck.second + 1 <= 7){
-			if (distmaze[cellcheck.first][cellcheck.second+1] < minval) {
-				minval = distmaze[cellcheck.first][cellcheck.second+1];
-		  next_ideal_pos.first = cellcheck.first;
-		  next_ideal_pos.second = cellcheck.second+1;
+//start of floodfill algorithm
+//checks.push(mouse_pos);
+while(mouse_pos != goal_coord){
+	checks.push(mouse_pos);
+	one_greater = false;
+	minval = -1;
+	changed_mouse_pos = false;
+	while(!checks.empty()){
+		cellcheck = checks.top();
+		checks.pop();
+		//assume making maze puts walls for all boundaries 
+		if (maze[cellcheck.first][cellcheck.second].left_wall != 1 && one_greater == false){
+			if (maze[cellcheck.first][cellcheck.second -1].value +1 ==  maze[mouse_pos.first][mouse_pos.second].value){
+				one_greater = true;
 			}
-		}
-		
-		if (cellcheck.first + 1 <= 7){
-			if (distmaze[cellcheck.first + 1][cellcheck.second] < minval) {
-				minval = distmaze[cellcheck.first+1][cellcheck.second];
-		  next_ideal_pos.first = cellcheck.first+1;
-		  next_ideal_pos.second = cellcheck.second;
-			}
-		}
-		
-		
-		distmaze[cellcheck.first][cellcheck.second] = minval ++;
-		
-		pair<int, int> pair_to_push;
-		
-		
-		if (cellcheck.second -1  >= 0){
-          pair_to_push.first = cellcheck.first;
-		  pair_to_push.second = cellcheck.second-1;
-		  checks.push(pair_to_push);
-        }
+			if (minval == -1){
 				
-        if (cellcheck.first -1 >= 0){
-		  pair_to_push.first = cellcheck.first-1;
-		  pair_to_push.second = cellcheck.second;
-		  checks.push(pair_to_push);
-        }
-		
-		if (cellcheck.second + 1 <= 7){
-		  pair_to_push.first = cellcheck.first;
-		  pair_to_push.second = cellcheck.second+1;
-		  checks.push(pair_to_push);
+				minval = maze[cellcheck.first][cellcheck.second-1].value;
+			}
+			else{
+				if ( maze[cellcheck.first][cellcheck.second-1].value < minval){
+					minval =  maze[cellcheck.first][cellcheck.second-1].value;
+				}
+			}
+		}
+		if (maze[cellcheck.first][cellcheck.second].right_wall != 1 && one_greater== false){
+			if (maze[cellcheck.first][cellcheck.second +1].value +1 ==  maze[mouse_pos.first][mouse_pos.second].value){
+				one_greater = true;
+			}
+			if (minval == -1){
+				minval = maze[cellcheck.first][cellcheck.second+1].value;
+			}
+			else{
+				if ( maze[cellcheck.first][cellcheck.second+1].value < minval){
+					minval =  maze[cellcheck.first][cellcheck.second+1].value;
+				}
+			}			
+		}
+		if (maze[cellcheck.first][cellcheck.second].top_wall != 1 && one_greater== false){
+			if (maze[cellcheck.first-1][cellcheck.second ].value +1 ==  maze[mouse_pos.first][mouse_pos.second].value){
+				one_greater = true;
+			}		
+			if (minval == -1){
+				minval = maze[cellcheck.first-1][cellcheck.second].value;
+			}
+			else{
+				if ( maze[cellcheck.first-1][cellcheck.second].value < minval){
+					minval =  maze[cellcheck.first][cellcheck.second+1].value;
+				}
+			}			
 		}
 		
-		if (cellcheck.first + 1 <= 7){
-		  pair_to_push.first = cellcheck.first + 1;
-		  pair_to_push.second = cellcheck.second;
-		  checks.push(pair_to_push);
+		if (maze[cellcheck.first][cellcheck.second].bottom_wall != 1 && one_greater== false){
+			if (maze[cellcheck.first+1][cellcheck.second ].value +1 ==  maze[mouse_pos.first][mouse_pos.second].value){
+				one_greater = true;
+			}
+			if (minval == -1){
+				minval = maze[cellcheck.first+1][cellcheck.second].value;
+			}
+			else{
+				if ( maze[cellcheck.first+1][cellcheck.second].value < minval){
+					minval =  maze[cellcheck.first][cellcheck.second+1].value;
+				}
+			}		
+		}		
+		//if cell check value isnt one greater than accessible neighbors
+		if (one_greater == false){
+			//cellcheck = minval + 1
+			maze[cellcheck.first][cellcheck.second].value = minval + 1;
+			//checks.push (neighbors)
+			if (maze[cellcheck.first][cellcheck.second].left_wall != 1){
+				pair_to_push_to_checks.first = cellcheck.first;
+				pair_to_push_to_checks.second = cellcheck.second -1;
+				checks.push(pair_to_push_to_checks);
+			}
+			if (maze[cellcheck.first][cellcheck.second].right_wall != 1){
+				pair_to_push_to_checks.first = cellcheck.first;
+				pair_to_push_to_checks.second = cellcheck.second +1;
+				checks.push(pair_to_push_to_checks);
+			}
+			if (maze[cellcheck.first][cellcheck.second].top_wall != 1){
+				pair_to_push_to_checks.first = cellcheck.first-1;
+				pair_to_push_to_checks.second = cellcheck.second;
+				checks.push(pair_to_push_to_checks);
+			}
+			if (maze[cellcheck.first][cellcheck.second].bottom_wall != 1){
+				pair_to_push_to_checks.first = cellcheck.first+1;
+				pair_to_push_to_checks.second = cellcheck.second;
+				checks.push(pair_to_push_to_checks);
+			}			
+			
 		}
 		
 		
-		start.first = next_ideal_pos.first;
-		start.second = next_ideal_pos.second;
-		
+			
+			
+		}
+		//advance to ideal neighbor
+		if (maze[mouse_pos.first][mouse_pos.second].left_wall != 1){
+			if(maze[mouse_pos.first][mouse_pos.second-1].value+1 == maze[mouse_pos.first][mouse_pos.second].value){
+				mouse_pos.first = mouse_pos.first;
+				mouse_pos.second = mouse_pos.second -1;
+				changed_mouse_pos = true;
+			}
+		}
+		if (maze[mouse_pos.first][mouse_pos.second].right_wall != 1 && changed_mouse_pos == false){
+			if(maze[mouse_pos.first][mouse_pos.second+1].value+1 == maze[mouse_pos.first][mouse_pos.second].value){
+				mouse_pos.first = mouse_pos.first;
+				mouse_pos.second = mouse_pos.second +1;
+				changed_mouse_pos = true;
+			}
+		}
+		if (maze[mouse_pos.first][mouse_pos.second].top_wall != 1 && changed_mouse_pos == false){
+			if(maze[mouse_pos.first-1][mouse_pos.second].value+1 == maze[mouse_pos.first][mouse_pos.second].value){
+				mouse_pos.first = mouse_pos.first-1;
+				mouse_pos.second = mouse_pos.second ;
+				changed_mouse_pos = true;
+			}
+		}
+		if (maze[mouse_pos.first][mouse_pos.second].bottom_wall != 1 && changed_mouse_pos == false){
+			if(maze[mouse_pos.first+1][mouse_pos.second].value+1 == maze[mouse_pos.first][mouse_pos.second].value){
+				mouse_pos.first = mouse_pos.first+1;
+				mouse_pos.second = mouse_pos.second ;
+				changed_mouse_pos = true;
+			}
+		}
+		//done going to next ideal position
+	}//while (mouse_pos != goal)
+	
+	return 0;	
+}
 
 
 
 
-              // if (distmaze[cellcheck.first][cellcheck.second] !=
-              //    distmaze[cellcheck.first + 1][cellcheck.second + 2] + 1 ){
-              //
-              // }
-            }
-            //cout << "CDCDCDCDCD" << endl;
-          }
 
-        }
 
-        //
-        // testing
-        //
-        // for(int height = 0;height < 8; ++height){
-        //   for(int width = 0; width < 8; ++width){
-        //     distmaze[height][width] = 1;
-        //   }
-        // }
-        //
-        // for(int height = 0;height < 8; ++height){
-        //   for(int width = 0; width < 8; ++width){
-        //     cout << distmaze[height][width] << " ";
-        //   }
-        //
-        //   cout << endl;
-        // }
+	
+	
+
+
+
+void display_maze(){
+
+	
+	
+}
